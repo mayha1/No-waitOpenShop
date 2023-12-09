fb89eefca1f1551624f0fe94e5c0e6413f4dd8cf
mayha1 <dinhhoangnhatminh2002@gmail.com>
2023-12-09 21:49:02 +0700

    add model 2

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def generateMatrix(size):
               for j in range(size[1])]).transpose(1, 0)
     return matrix
 
-
 class Model1:
     def __init__(self):
         self.model = gb.Model()
@@ -44,7 +43,12 @@ class Model1:
         # change objective
         self.model.setObjective(self.makespan, GRB.MINIMIZE)
         self.model.optimize()
+        print("timeMatrix", self.timeMatrix)
+        print("x", self.x)
+        print("y", self.y)
         print("c: ", self.c.X)
+        print("s", self.s.X)
+        print(self.c.X[3,1], "------", self.s.X[3],"---------", self.timeMatrix[3,1])
         print("makespan: ", self.makespan.X)
 
         print(f"-----------Model runtime: {self.model.runtime}")
@@ -89,9 +93,9 @@ class Model1:
     # 6 & 7
     def addSucceedDummyConstraint(self):
         for j in range(self.nJobs):
-            self.model.addConstr(self.x[j, :, 0].sum() == 1)
+            self.model.addConstr(self.x[j, :, self.nMachines].sum() == 1)
         for i in range(self.nMachines):
-            self.model.addConstr(self.y[:, i, 0].sum() == 1)
+            self.model.addConstr(self.y[:, i, self.nJobs].sum() == 1)
 
     # 8 & 9
     def addNotAllSucceedPreceedConstraint(self):
@@ -106,9 +110,12 @@ class Model1:
     def addNoWaitConstraint(self):
         for i in range(self.nMachines):
             for j in range(self.nJobs):
-                self.c[j,i] <= self.s[j] + self.timeMatrix[j, :].sum()
-                self.c[j,i] >= self.s[j] + self.timeMatrix[j, i]
-
+                self.model.addConstr(
+                    self.c[j,i] <= self.s[j] + self.timeMatrix[j, :].sum()
+                )
+                self.model.addConstr(
+                    self.c[j,i] >= self.s[j] + self.timeMatrix[j, i]
+                )
     # 12 & 13
     def addStartingConstraint(self):
         M = 100*100
@@ -124,13 +131,133 @@ class Model1:
             for i in range(self.nMachines):
                 self.model.addConstr(self.makespan >= self.c[j, i])
 # Add variables
+class Model2:
+    def __init__(self):
+        self.model = gb.Model()
+
+    def solve(self, matrix, nJobs, nMachines):
+
+        self.timeMatrix = matrix
+
+        # Add variables
+        self.nMachines = nMachines
+        self.nJobs = nJobs
+        self.addX()
+        self.addY()
+        self.addC()
+        self.addS()
+        self.addMakespan()
+
+        self.addScheduleOnceConstraint()
+        self.addOneOrderPerOperationConstraint()
+        self.addNoWaitConstraint()
+        self.addOrderConstraint()
+        self.addMakespan()
+
+        # Add objective
+        # self.model.setObjective(0, GRB.MINIMIZE)
+        # self.model.optimize()
+
+        # change objective
+        self.model.setObjective(self.makespan, GRB.MINIMIZE)
+        self.model.optimize()
+        print("timeMatrix", self.timeMatrix)
+        print("x", self.x)
+        print("y", self.y)
+        # print("c: ", self.c.X)
+        # print("s", self.s.X)
+        # print("makespan: ", self.makespan.X)
+
+        print(f"-----------Model runtime: {self.model.runtime}")
+
+    def addX(self):
+        self.x = self.model.addMVar((self.nJobs, self.nMachines, self.nJobs), vtype=gb.GRB.BINARY, name="x")
+
+    def addY(self):
+        self.y = self.model.addMVar((self.nJobs, self.nMachines, self.nMachines), vtype=gb.GRB.BINARY, name="y")
+    
+    def addC(self):
+        self.c = self.model.addMVar((self.nJobs, self.nMachines), vtype=gb.GRB.INTEGER, name="c")
+    
+    def addS(self):
+        self.s = self.model.addMVar(self.nJobs, vtype=gb.GRB.INTEGER, name="s")
+
+    def addMakespan(self):
+        self.makespan = self.model.addMVar(1, vtype=gb.GRB.INTEGER, name="makespan")
+
+    # 17
+    def addScheduleOnceConstraint(self):
+        for j in range (self.nJobs):
+            for i in range (self.nMachines):
+                self.model.addConstr(
+                    self.x[j, i, :].sum() == 1
+                )
+                self.model.addConstr(
+                    self.y[j, i, :].sum() == 1
+                )
+    
+    # 18 & 19 
+    def addOneOrderPerOperationConstraint(self):
+        for j in range(self.nJobs):
+            for k in range(self.nJobs):
+                self.model.addConstr(
+                    self.x[j, :, k].sum() == 1
+                )
+        for i in range(self.nMachines):
+            for l in range(self.nMachines):
+                self.model.addConstr(
+                    self.y[:, i, l].sum() == 1
+                )
+
+    # 20 & 21
+    def addNoWaitConstraint(self):
+        for i in range(self.nMachines):
+            for j in range(self.nJobs):
+                self.model.addConstr(
+                    self.c[j,i] <= self.s[j] + self.timeMatrix[j, :].sum()
+                )
+                self.model.addConstr(
+                    self.c[j,i] >= self.s[j] + self.timeMatrix[j, i]
+                )
+
+    # 22 & 23
+    def addOrderConstraint(self):
+        M = 100*100
+        for j in range(self.nJobs):
+            for i in range(self.nMachines):
+                for r in range(self.nMachines):
+                    for k in range(1, self.nJobs):
+                        self.model.addConstr(
+                            self.c[j, i] >= self.c[j, r] + self.timeMatrix[j, i] - (1 - self.x[j, i, k]) * M
+                            - (1 - self.x[[j], :, :][:, [r], :][:, :, [t for t in range(k-1)]] )
+                        )
+                for e in range(self.nJobs):
+                    for l in range(1, self.nMachines):
+                        self.model.addConstr(
+                            self.c[j, i] >= self.c[e, i] + self.timeMatrix[j, i] - (1 - self.y[j, i, l]) * M
+                            - (1 - self.y[[e], :, :][:, [i], :][:, :, [t for t in range(l-1)]] )
+                        )
+    
+    def addMakespanConstraint(self):
+        for j in range(self.nJobs):
+            for i in range(self.nMachines):
+                self.model.addConstr(self.makespan >= self.c[j, i])
+
+            
+
+
 
 #generate Data
 sizes = [[3, 3], [3, 4], [4, 3], [4, 4], [4, 5], [5, 4], [5, 5], [5, 6], [6, 5], [6, 6]]
 nInstances = 2
-for size in sizes:
-    for i in range(nInstances):
-        matrix = generateMatrix(size)
-        model1 = Model1()
-        model1.solve(matrix, size[0], size[1])
-        # breakpoint()
+# for size in sizes:
+size = [4,3]
+for i in range(nInstances):
+    matrix = generateMatrix(size)
+    # model 1
+    # model1 = Model1()
+    # model1.solve(matrix, size[0], size[1])
+    # breakpoint()
+    # model 2
+    model2 = Model2()
+    model2.solve(matrix, size[0], size[1])
